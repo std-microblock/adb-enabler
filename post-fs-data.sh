4856660804a29372b26055da75a5c06228b4c3af
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2024-08-16 14:31:30 +0800

    support miui
    Co-Authored-By: Howard Wu <40033067+howard20181@users.noreply.github.com>

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,4 +1,7 @@
 setprop persist.service.adb.enable 1
 setprop persist.service.debuggable 1
 setprop persist.sys.usb.config mtp,adb
-setprop ctl.restart adbd
+setprop persist.security.adbinput 1
+setprop persist.security.adbinstall 1
+setprop sys.usb.adb.disabled 0
+setprop sys.usb.config mtp,adb
