a94f2ea6b8f6640526455e0c6d0f1457f77e175e
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2024-08-16 14:02:36 +0800

    update
    Co-Authored-By: Howard Wu <40033067+howard20181@users.noreply.github.com>

---
+++ post-fs-data.sh
@@ -0,0 +1,4 @@
+setprop persist.service.adb.enable 1
+setprop persist.service.debuggable 1
+setprop persist.sys.usb.config mtp,adb
+setprop ctl.restart adbd
--- service.sh
+++
@@ -1,7 +0,0 @@
-resetprop -n persist.service.adb.enable 1
-resetprop -n ro.debuggable 1
-resetprop -n ro.secure 0
-resetprop -n ro.adb.secure 0
-
-stop adbd
-start adbd
